b7ff4470ab182468385c483f5e087f53c3f2fb27
Tokisaki Kurumi <minhkhoile2001nsg@gmail.com>
2019-12-13 17:28:11 +0700

    Finish the content of 4 types of neural network

--- work_concept/js/card_reveal.js
+++ work_concept/js/card_reveal.js
@@ -1,7 +1,30 @@
 $(function(){
-	$(".my_card_heading").click(function(){
-		$(".my_card_heading").toggleClass("semi_rounded_top");
-		$(".my_card_content").slideToggle("slow");
-		$(".my_card_content").toggleClass("semi_rounded_btm");
+	/*
+	* TODO:
+	*	- Use loop and array to reduce hard-code
+	*	- Click on 1 of the el#id will slide down its content and slide in other contents
+	*/
+	$("#single_layer").click(function(){
+		$("#single_layer > .my_card_heading").toggleClass("semi_rounded_top");
+		$("#single_layer > .my_card_content").slideToggle("slow");
+		$("#single_layer > .my_card_content").toggleClass("semi_rounded_btm");
+	});
+
+	$("#multi_layer").click(function(){
+		$("#multi_layer > .my_card_heading").toggleClass("semi_rounded_top");
+		$("#multi_layer > .my_card_content").slideToggle("slow");
+		$("#multi_layer > .my_card_content").toggleClass("semi_rounded_btm");
+	});
+
+	$("#cnn").click(function(){
+		$("#cnn > .my_card_heading").toggleClass("semi_rounded_top");
+		$("#cnn > .my_card_content").slideToggle("slow");
+		$("#cnn > .my_card_content").toggleClass("semi_rounded_btm");
+	});
+
+	$("#rnn").click(function(){
+		$("#rnn > .my_card_heading").toggleClass("semi_rounded_top");
+		$("#rnn > .my_card_content").slideToggle("slow");
+		$("#rnn > .my_card_content").toggleClass("semi_rounded_btm");
 	});
 });
