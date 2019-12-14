1258d168d5606e6e2bfc5be344225d844f116fc0
Tokisaki Kurumi <minhkhoile2001nsg@gmail.com>
2019-12-14 10:08:21 +0700

    Add the code and animation for token, pos, dep

--- work_concept/js/card_reveal.js
+++ work_concept/js/card_reveal.js
@@ -1,7 +1,6 @@
 $(function(){
 	/*
 	* TODO:
-	*	- Use loop and array to reduce hard-code
 	*	- Click on 1 of the el#id will slide down its content and slide in other contents
 	*/
 
@@ -15,7 +14,32 @@ $(function(){
 		});
 	}
 
+	function reveal_card_code(card_element)
+	{
+		$(card_element).click(function(){
+			var card_heading_el = card_element + " > .my_card_heading";
+			var card_content_el = card_element + " > .my_card_content";
+			var card_footer_el = card_element + " > .my_card_footer";
+			$(card_heading_el).toggleClass("semi_rounded_top");
+			$(card_content_el).slideToggle("slow");
+			$(card_footer_el).slideToggle("slow").toggleClass("semi_rounded_btm");
+		});
+	}
+
+	function run_code(code_btn)
+	{
+		$(code_btn).click(function(event){
+			var code_res = code_btn + " ~ p.code_result";
+			event.stopPropagation();
+			$(code_res).slideToggle("slow");
+		});
+	}
+
 	var card = new Array("#single_layer", "#multi_layer", "#cnn", "#rnn", "#speech_recog");
+	var card_have_code = new Array("#tokenization", "#pos", "#dep");
+	var btn_run_code = new Array("#token_run", "#pos_run", "#dep_run");
 
 	card.forEach(reveal_card_content);
+	card_have_code.forEach(reveal_card_code);
+	btn_run_code.forEach(run_code);
 });
